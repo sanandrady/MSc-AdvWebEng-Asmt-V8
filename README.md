008407e5a05329d5f612dc9160d3c537e18856a7
sanand.rady <sanand.rady@googlemail.com>
2015-10-17 22:11:25 +0100

    Registration form

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# PATeRefApp-v8
-### Patient eReferral MEAN Stack Application
+# update
 
 This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
 version 0.11.1.
---
+++ app/scripts/controllers/joinin.js
@@ -0,0 +1,42 @@
+﻿'use strict';
+
+
+  //  angular.module('pateRefAppV8App')
+  //.controller('JoinCtrl', function ($scope, $routeProvider) {
+  //    $scope.join = $routeProvider
+  //    .when('/joinin', {
+  //        templateUrl: 'views/joinin.html',
+  //        controller: 'JoinCtrl'
+  //    })
+  //    .otherwise({
+  //        templateUrl: 'views/joinin.html',
+  //        controller: 'JoinCtrl'
+  //    });
+
+//});
+
+var patApp = angular.module('pateRefAppV8App', []);
+
+patApp.controller('JoinCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    console.log('Hello World from JoinCtrl');
+
+    var refresh = function () {
+        $http.get('/pmi').success(function (response) {
+            console.log('I got the data');
+            $scope.pmi = response;
+            $scope.user = '';
+        });
+    };
+
+    refresh();
+
+    $scope.addUser = function () {
+        console.log($scope.user);
+        $http.post('/pmi', $scope.user).success(function (response) {
+            console.log(response);
+            refresh();
+
+        });
+    };
+}]);
