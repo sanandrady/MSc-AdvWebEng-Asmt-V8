1b35946e7cd7db4bf4738b462fe6ed8a57308785
sanand.rady <sanand.rady@googlemail.com>
2015-10-18 11:01:15 +0100

    First Commit - The Environment

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/meanappv8-dev'
+  },
+
+  seedDB: true
+};
